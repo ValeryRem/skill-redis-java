9abb03f3da0e74797d6e3dd5d71ef5783bf28cc2
Valery Remenyuk <remenyuk79@gmail.com>
2020-03-26 10:55:23 +0300

    Experiments with testGetShortestRoute().

--- 08_ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RoadCalculatorTest.java
+++ 08_ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RoadCalculatorTest.java
@@ -36,6 +36,9 @@ public class RoadCalculatorTest {
     StationIndex stationIndex = new StationIndex();
     RouteCalculator routeCalculator = new RouteCalculator(stationIndex);
     List<Station> stationList = new ArrayList<>();
+    private static final double ROUTE_TIME = 2.5;
+    private static final double CONNECTION_TIME = 3.5;
+    private static final double DELTA = 0.00001;
 
     @Before
     public void setUp(){
@@ -44,97 +47,99 @@ public class RoadCalculatorTest {
         Line line3 = new Line(3, "Line 3");
         Line line4 = new Line(4, "Line 4");
 
-        Station A = new Station( "A", line1);
-        Station B = new Station("B", line1);
-        Station C = new Station("C", line1);
-        Station D = new Station("D", line1);
-        Station E = new Station("E", line1);
-        Station F = new Station("F", line2);
-        Station G = new Station("G", line2);
-        Station H = new Station("H", line2);
-        Station J = new Station("J", line3);
-        Station K = new Station("K", line3);
-        Station L = new Station("L", line3);
-        Station M = new Station("M", line4);
-        Station N = new Station("N", line4);
+        Station A1 = new Station( "A1", line1);
+        Station B1 = new Station("B1", line1);
+        Station C1 = new Station("C1", line1);
+        Station D1 = new Station("D1", line1);
+        Station E1 = new Station("E1", line1);
+        Station F2 = new Station("F2", line2);
+        Station G2 = new Station("G2", line2);
+        Station H2 = new Station("H2", line2);
+        Station J3 = new Station("J3", line3);
+        Station K3 = new Station("K3", line3);
+        Station L3 = new Station("L3", line3);
+        Station M4 = new Station("M4", line4);
+        Station N4 = new Station("N4", line4);
 
-        line1.addStation(A);
-        line1.addStation(B);
-        line1.addStation(C);
-        line1.addStation(D);
-        line2.addStation(F);
-        line2.addStation(G);
-        line2.addStation(H);
-        line3.addStation(J);
-        line3.addStation(K);
-        line3.addStation(L);
-        line4.addStation(M);
-        line4.addStation(N);
+        line1.addStation(A1);
+        line1.addStation(B1);
+        line1.addStation(C1);
+        line1.addStation(D1);
+        line2.addStation(F2);
+        line2.addStation(G2);
+        line2.addStation(H2);
+        line3.addStation(J3);
+        line3.addStation(K3);
+        line3.addStation(L3);
+        line4.addStation(M4);
+        line4.addStation(N4);
 
         stationIndex.addLine(line1);
         stationIndex.addLine(line2);
         stationIndex.addLine(line3);
         stationIndex.addLine(line4);
 
-        stationList = Arrays.asList(A, B, C, D, E, F, G, H, J, K, L, M, N);
+        stationList = Arrays.asList(A1, B1, C1, D1, E1, F2, G2, H2, J3, K3, L3, M4, N4);
         stationList.forEach(station ->  stationIndex.addStation(station));
-        stationIndex.addConnection(Arrays.asList(B, F));
-        stationIndex.addConnection(Arrays.asList(D, J));
-        stationIndex.addConnection(Arrays.asList(K, M));
+        stationIndex.addConnection(Arrays.asList(B1, F2));
+        stationIndex.addConnection(Arrays.asList(D1, J3));
+        stationIndex.addConnection(Arrays.asList(K3, M4));
     }
 
     @Test
     public void testCalculateDuration() {
-        stationList = makeRoute("BCDJK");
+        stationList = makeRoute("B1-C1-D1-J3-K3");
         double expected = RouteCalculator.calculateDuration(stationList);
-        double actual = 11.0;
-        assertEquals(expected, actual, 0.0);
+        double actual = ROUTE_TIME * 2 + CONNECTION_TIME + ROUTE_TIME;
+        assertEquals(expected, actual, DELTA);
     }
 
     @Test
     public void testGetRouteOnTheLine () {
-        List<Station> actual = makeRoute("JKL");
-        List<Station> expected =  routeCalculator.getRouteOnTheLine (stationIndex.getStation("J"), stationIndex.getStation("L"));
-        assertEquals("Ожидается путь J-K-L", expected, actual);
+        List<Station> actual = makeRoute("J3-K3-L3");
+        List<Station> expected =  routeCalculator.getRouteOnTheLine (stationIndex.getStation("J3"), stationIndex.getStation("L3"));
+        assertEquals("Ожидается путь J3-K3-L3", expected, actual);
     }
 
     @Test
     public void testGetRouteWithOneConnection() {
-        List<Station> actual  = makeRoute("CDJKL");
-        List<Station> expected = routeCalculator.getRouteWithOneConnection(stationIndex.getStation("C"), stationIndex.getStation("L"));
-        assertEquals("Ожидается путь  C D J K L", expected, actual);
+        List<Station> actual  = makeRoute("C1-D1-J3-K3-L3");
+        List<Station> expected = routeCalculator.getRouteWithOneConnection(stationIndex.getStation("C1"), stationIndex.getStation("L3"));
+        assertEquals("Ожидается путь  C1-D1-J3-K3-L3", expected, actual);
     }
 
     @Test
     public void testGetRouteWithTwoConnections () {
-        List<Station> actual  = makeRoute("GFBCDJK");
-        List<Station> expected = routeCalculator.getRouteWithTwoConnections(stationIndex.getStation("G"), stationIndex.getStation("K"));
-        assertEquals("Ожидается путь  G F B C D J K", expected, actual);
+        List<Station> actual  = makeRoute("G2-F2-B1-C1-D1-J3-K3");
+        List<Station> expected = routeCalculator.getRouteWithTwoConnections(stationIndex.getStation("G2"), stationIndex.getStation("K3"));
+        assertEquals("Ожидается путь  G2-F2-B1-C1-D1-J3-K3", expected, actual);
     }
 
     @Test
     public void testGetRouteViaConnectedLine() {
-        List<Station> actual  = makeRoute("ABCDJK");
-        List<Station> expected =  routeCalculator.getRouteViaConnectedLine(stationIndex.getStation("A"), stationIndex.getStation("K"));
-        assertEquals("Ожидается путь A B C D J K", expected, actual);
+        List<Station> actual  = makeRoute("D1-J3");
+        List<Station> expected =  routeCalculator.getRouteViaConnectedLine(stationIndex.getStation("D1"), stationIndex.getStation("J3"));
+        assertEquals("Ожидается путь D1-J3", expected, actual);
     }
 
     @Test
     public void testGetShortestRoute () {
-        List<Station> actual = makeRoute("GFBCDJK");
-        List<Station> expected =  routeCalculator.getShortestRoute (stationIndex.getStation("G"), stationIndex.getStation("K"));
-        assertEquals("Ожидается путь G F B C D J K", expected, actual);
+        List<Station> actual = makeRoute("G2-F2-B1-C1-D1-J3-K3");
+        List<Station> expected =  routeCalculator.getShortestRoute (stationIndex.getStation("G2"), stationIndex.getStation("K3"));
+        assertEquals("Ожидается путь G2-F2-B1-C1-D1-J3-K3", expected, actual);
     }
 
     @After
     public void tearDown(){
         stationList = null;
+        stationIndex = null;
+        routeCalculator = null;
     }
 
     private List<Station> makeRoute(String namesJoint) {
         List<Station> routeExpected = new ArrayList<>();
-        for (int i = 0; i < namesJoint.length(); i++) {
-            String name = String.valueOf(namesJoint.charAt(i));
+        for (int i = 0; i < namesJoint.split("-").length; i++) {
+            String name = namesJoint.split("-")[i];
             routeExpected.add(stationIndex.getStation(name));
         }
         return routeExpected;
