7ad37515b1c6468f5c528c9561cf496f22ab0fc4
Valery Remenyuk <remenyuk79@gmail.com>
2020-03-30 14:02:23 +0300

    Added check of null for the list of files. Variable output set up.

--- 09_FilesAndNetwork/src/com/company/Main.java
+++ 09_FilesAndNetwork/src/com/company/Main.java
@@ -1,22 +1,31 @@
 package com.company;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 /**
  * Написать программу, которая будет измерять размер всего содержимого папки, путь которой передаётся на вход,
  * и выводить его в удобочитаемом виде — в байтах, килобайтах, мегабайтах или гигабайтах.
+ * For example: C:/Users/valery/Desktop/Android
  */
-public class Main {
 
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static final long MEGABYTE = 1000000;
+    static final long GIGABYTE = MEGABYTE*1000;
     public static void main(String[] args) {
-        File dir = new File("C:/Users/valery/Desktop/Android");
-        System.out.println("Size of the directory C:/Users/valery/Desktop/Android: " + getDirSize(dir)/1000000 + " mb");
+        System.out.println("Input path to your directory");
+        String path = scanner.nextLine();
+        File dir = new File(path);
+        long size = getDirSize(dir);
+        System.out.println("Size of the directory " + path + ": ");
+        if (size <= MEGABYTE) {
+            System.out.println(size/1000 + " Kb");
+        } else if (size > MEGABYTE && size <= GIGABYTE) {
+            System.out.println(size/MEGABYTE + " Mb");
+        } else {
+            System.out.println(size/GIGABYTE + " Gb");
+        }
     }
 
     private static long getDirSize(File dir) {
@@ -25,17 +34,20 @@ public class Main {
             size = dir.length();
         } else {
             File[] subFiles = dir.listFiles();
-            try {
-                for (File file : subFiles) {
-                    if (file.isFile()) {
-                        size += file.length();
-                    } else {
-                        size += getDirSize(file);
+            if (subFiles != null) {
+                try {
+                    for (File file : subFiles) {
+                        if (file.isFile()) {
+                            size += file.length();
+                        } else {
+                            size += getDirSize(file);
+                        }
                     }
+                } catch (NullPointerException ex) {
+                    ex.printStackTrace();
                 }
-            }
-            catch (NullPointerException ex) {
-                ex.printStackTrace();
+            } else {
+                System.out.println("The directory is absent.");
             }
         }
         return size;
