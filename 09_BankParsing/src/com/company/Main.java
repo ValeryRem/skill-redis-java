d9eed13a546abfb3123c67cb20dea623d3526c07
Valery Remenyuk <remenyuk79@gmail.com>
2020-04-04 12:30:00 +0300

    Statistic collection is separated of parsing.

--- 09_BankParsing/src/com/company/Main.java
+++ 09_BankParsing/src/com/company/Main.java
@@ -14,40 +14,24 @@ public class Main {
 
     public static void main(String[] args) {
         String csvFile = "C:\\Users\\valery\\Desktop\\java_basics\\09_BankParsing\\src\\com\\movementList.csv";
-        int indexOfSum;
-        List<ArrayList<String>> incomeList = new ArrayList<>();
-        List<ArrayList<String>> expenseList = new ArrayList<>();
+        List<List<String>> incomeList = new ArrayList<>();
+        List<List<String>> expenseList = new ArrayList<>();
         TreeMap<String, Double> expenseTargets = new TreeMap<>();
         try {
             Scanner scanner = new Scanner(new File(csvFile));
-            String lastWord;
-            double sumToAdd;
             while (scanner.hasNext()) {
-                ArrayList<String> stringList = parseLine(scanner.nextLine());
+                List<String> stringList = parseLine(scanner.nextLine());
                 if (stringList.contains("ALFA_MOBILE>MOSCOW")) {
                     incomeList.add(stringList);
-                    indexOfSum = stringList.lastIndexOf("RUR") - 1;
-                    totalIncome += Double.parseDouble(stringList.get(indexOfSum));
                 } else {
                     expenseList.add(stringList);
                 }
             }
-
-            for (int i = 1; i < expenseList.size(); i++) {
-                lastWord = expenseList.get(i).get(expenseList.get(i).size() - 1);
-                if (lastWord.contains("\"")) {
-                    sumToAdd = Double.parseDouble(lastWord.split("\"")[1].replace(",", "."));
-                } else {
-                    sumToAdd = Double.parseDouble(lastWord.split(",")[lastWord.split(",").length - 1]);
-                }
-                if (!expenseTargets.containsKey(expenseList.get(i).get(2))) {
-                    expenseTargets.put(expenseList.get(i).get(2), sumToAdd);
-                } else {
-                    expenseTargets.put(expenseList.get(i).get(2), expenseTargets.get(expenseList.get(i).get(2)) + sumToAdd);
-                }
-                totalExpenses += sumToAdd;
-            }
             scanner.close();
+
+            getSumOfIncome(incomeList);
+            getSumOfExpenses(expenseList, expenseTargets);
+
             System.out.printf("%s%.2f%s%n", "Total Income: ", totalIncome, " RUR");
             System.out.printf("%s%.2f%s%n", "Total Expenses: ", totalExpenses, " RUR");
             System.out.println("incomeList.size(): " + incomeList.size());
@@ -58,19 +42,50 @@ public class Main {
             for (Map.Entry<String, Double> entry : expenseTargets.entrySet()) {
                 System.out.println(entry.getKey() + " = " + entry.getValue() + " RUR");
             }
-        } catch (FileNotFoundException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private static ArrayList<String> parseLine(String csvLine) throws NullPointerException {
-        ArrayList<String> result = new ArrayList<>();
+    private static List<String> parseLine(String csvLine) {
         if (csvLine.matches("[\\w\\W]+")) {
-            String[] words = csvLine.split("\\s+");
-            Collections.addAll(result, words);
+            return  Arrays.asList(csvLine.split("\\s+"));
         } else {
             System.err.println("Empty line!");
+            return Collections.emptyList();
         }
-        return result;
+//        ArrayList<String> result = new ArrayList<>();
+//        if (csvLine.matches("[\\w\\W]+")) {
+//            String[] words = csvLine.split("\\s+");
+//            Collections.addAll(result, words);
+//        } else {
+//            System.err.println("Empty line!");
+//        }
+//        return result;
+    }
+
+    private static void getSumOfIncome (List<List<String>> incomeList ) {
+        incomeList.forEach(strings -> {
+            totalIncome += Double.parseDouble(strings.get(strings.lastIndexOf("RUR") - 1));
+        });
+    }
+
+    private static void getSumOfExpenses(List<List<String>> expenseList, TreeMap<String, Double> expenseTargets) {
+        String lastWord;
+        double sumToAdd;
+        for (int i = 1; i < expenseList.size(); i++) {
+            lastWord = expenseList.get(i).get(expenseList.get(i).size() - 1);
+            if (lastWord.contains("\"")) {
+                sumToAdd = Double.parseDouble(lastWord.split("\"")[1].replace(",", "."));
+            } else {
+                sumToAdd = Double.parseDouble(lastWord.split(",")[lastWord.split(",").length - 1]);
+            }
+            if (!expenseTargets.containsKey(expenseList.get(i).get(2))) {
+                expenseTargets.put(expenseList.get(i).get(2), sumToAdd);
+            } else {
+                expenseTargets.put(expenseList.get(i).get(2), expenseTargets.get(expenseList.get(i).get(2)) + sumToAdd);
+            }
+            totalExpenses += sumToAdd;
+        }
     }
 }
