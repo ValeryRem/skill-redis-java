b7559ddffce8c3cbdbba218ebb27b6bba181c678
Valery Remenyuk <remenyuk79@gmail.com>
2020-04-15 17:30:31 +0300

    List of stations parsed to console.

--- 09_MoscowMetroJson/src/com/company/Main.java
+++ 09_MoscowMetroJson/src/com/company/Main.java
@@ -1,9 +1,9 @@
 /**
- * Написать код парсинга страницы Википедии “Список станций Московского метрополитена”, на основе этой страницы
+ * 1) Написать код парсинга страницы Википедии “Список станций Московского метрополитена”, который будет
  * создавать JSON-файл со списком станций по линиям и списком линий по формату JSON-файла из проекта SPBMetro (файл map.json,
  * приложен к домашнему заданию)
  * Также пропарсить и вывести в JSON-файл переходы между станциями.
- * Написать код, который прочитает созданный JSON-файл и напечатает количества станций на каждой линии.
+ * 2) Написать код, который прочитает созданный JSON-файл и напечатает количества станций на каждой линии.
  */
 
 package com.company;
@@ -12,27 +12,59 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.*;
-import java.net.URL;
-import java.nio.file.*;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
-    static String filePath = "src/com/company/text.txt";
-    static String origin = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0#%D0%A1%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B8_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
+
     public static void main(String[] args) {
-        try (FileWriter writer = new FileWriter(filePath, true)){
-            Document doc = Jsoup.connect(origin).maxBodySize (500_000).get();
-            Elements elements = doc.getElementsByTag("span");
-            int i = 0;
-            for (Element el : elements) {
-                String title = el.absUrl("title");
-                writer.append(title).append("\n");
-                try (InputStream in = new URL(title).openStream()) {
-                    Files.copy(in, Paths.get(filePath + i++ + ".txt"), StandardCopyOption.REPLACE_EXISTING);
+        String origin = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9" +
+                "_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1" +
+                "%82%D0%B5%D0%BD%D0%B0#%D0%A1%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B8_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_" +
+                "%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
+        Map<String, Set<String>> listMap = new HashMap<>();
+        parsMetroLines(origin, listMap);
+        listMap.entrySet().forEach(System.out::println);
+        System.out.println("\nNumber of lines: " + listMap.size());
+    }
+
+    private static void parsMetroLines (String origin, Map<String, Set<String>> listMap) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(origin).maxBodySize(2_000_000).get();
+            Elements rows = doc.select("tr");
+            for (Element row : rows) {
+                Elements elements = row.getElementsByTag("a");
+                Set<String> hashSet = new HashSet<>();
+                String key = "";
+                for (Element element : elements) {
+                    String title = element.attr("title");
+                    if (key.isEmpty() && title.contains("линия") && title.split(" ").length > 1 && title.split(" ").length < 4) {
+                        key = title;
+                    }
+                    String value = "";
+                    if (title.contains("станция метро")) {
+                        value = title.replaceFirst("\\(.*\\)", "").trim();
+                    }
+                    if (title.contains("Переход на станцию")) {
+                        value = "\t* " + title;
+                    }
+                    if (listMap.containsKey(key)) {
+                        listMap.get(key).add("\n\t\t" + value);
+                    } else  {
+                        if (key.length() > 0 && value.length() > 0) {
+                            hashSet.add("\n\t\t" + value);
+                            listMap.put(key, hashSet);
+                        }
+                    }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (IOException ex) {
-            ex.printStackTrace();
-        }    }
+    }
+//    private void jsonCreator (Map<String, Set<String>> map) {
+//        JSONObject json = new JSONObject();
+//        json.putAll( map );
+//    }
 }
