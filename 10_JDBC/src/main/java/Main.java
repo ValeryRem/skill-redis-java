91fa7410b0f34230fa0facabd482988306db68c2
Valery Remenyuk <remenyuk79@gmail.com>
2020-05-05 12:09:23 +0300

    Cyrillic name of new class expressed in '?' signs.

--- 10_JDBC/src/main/java/Main.java
+++ 10_JDBC/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -12,10 +13,18 @@ public class Main {
         Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
         Session session = sessionFactory.openSession();
-        Course course = session.get(Course.class, 10);
-        System.out.println(course.getName());
-        Students students = session.get(Students.class, 5);
-        System.out.println(students.getId() + ". " + students.getName() + ". " + students.getRegistrationDate());
+        Transaction transaction = session.beginTransaction();
+//        Course course = new Course();
+        Course course = session.get(Course.class, 49);
+        course.setName("Самый новый курс");
+        course.setType(CourseType.DESIGN);
+//        course.setTeacherId(1);
+        session.save(course);
+        transaction.commit();
+//        Course course = session.get(Course.class, 10);
+//        System.out.println(course.getName());
+//        Students students = session.get(Students.class, 5);
+//        System.out.println(students.getId() + ". " + students.getName() + ". " + students.getRegistrationDate());
         sessionFactory.close();
     }
 }
