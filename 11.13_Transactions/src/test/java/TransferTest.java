24e0da1ad26a3ee60342dc4a19effdd0792e9f31
Valery Remenyuk <remenyuk79@gmail.com>
2020-06-06 01:46:50 +0300

    Testing BalanceOfTransfers and recursive transfer test.

--- 11.13_Transactions/src/test/java/TransferTest.java
+++ 11.13_Transactions/src/test/java/TransferTest.java
@@ -10,7 +10,7 @@ import main.java.Bank;
 public class TransferTest {
     private static final double DELTA = 0.00001;
     private final static int numberOfAccounts = 10;
-    private HashMap<Integer, Account> accounts;
+    private HashMap<Integer, Account> accounts;// = new HashMap<>();
     private int fromAccountNum;
     private int toAccountNum;
     private long amount;
@@ -18,12 +18,15 @@ public class TransferTest {
     @Before
     public void setUp() {
        Bank.getHashMapOfAccounts(numberOfAccounts);
+       accounts = Bank.getAccounts();
     }
     @Test
     public void testBalanceOfTransfers() {
+        System.out.println("Bank accounts and final balance:");
+        accounts.entrySet().forEach(x -> System.out.println(x.getKey() + " - " + x.getValue().getBalance()));
         long initBalance = Bank.getTotalBalance();
         try {
-            for (int i = 1; i < 100; i++) {
+            for (int i = 1; i < 10; i++) {
                 fromAccountNum = (int) (Math.random()*10 + 1);
                 toAccountNum = (int) (Math.random()*10 + 1);
                 amount = (long) (100000*Math.random());
@@ -33,8 +36,7 @@ public class TransferTest {
             e.printStackTrace();
         }
         long finBalance = Bank.getTotalBalance();
-        System.out.println("Bank accounts and final balance:");
-        accounts.entrySet().forEach(x -> System.out.println(x.getKey() + " - " + x.getValue().getBalance()));
+
         assertEquals(initBalance, finBalance, DELTA);
     }
 
