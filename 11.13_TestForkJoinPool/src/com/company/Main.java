c0c7aaf335d797608d6e112304d7a330bbe90563
Valery Remenyuk <remenyuk79@gmail.com>
2020-06-23 10:46:03 +0300

    Universal code for different size sites.

--- 11.13_TestForkJoinPool/src/com/company/Main.java
+++ 11.13_TestForkJoinPool/src/com/company/Main.java
@@ -6,46 +6,46 @@ import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    private static final String prefix = "https://lenta.ru"; //"https://secure-headland-59304.herokuapp.com"; //"https://skillbox.ru"; //
+    private static final String prefix = "https://secure-headland-59304.herokuapp.com"; //"https://skillbox.ru"; //"https://lenta.ru"; //
     private static final String url = prefix + "/";
-    private static ResultStore resultStore = new ResultStore();
-    private static Parser parser = new Parser(url, prefix);
 
     public static void main(String[] args) {
         long from = System.currentTimeMillis();//new Date().getTime();
-        new ForkJoinPool().invoke(parser);//ForkJoinPool.commonPool().invoke(parser); //
+        Parser parser = new Parser(from, url, prefix);
+        ResultStore resultStore = new ResultStore(url);
+        ForkJoinPool.commonPool().invoke(parser); //new ForkJoinPool().invoke(parser);//
         try {
-            output(from, resultStore.getResult());
+            resultStore.output(from, resultStore.getResult());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void output(long from, Set<String> set) throws IOException {
-        Set<String> result = new TreeSet<>(set);
-        List<String> list = new ArrayList<>();
-        list.add(url);
-        FileWriter writer = new FileWriter("src\\com\\company\\output.txt");
-        for (String s : result) {
-            String span = "";
-            String shift = "    ";
-            int coeff = s.split("/").length - url.split("/").length;
-            for (int i = 0; i < coeff; i++) {
-                span += shift ;
-            }
-            list.add(span + s);
-        }
-        for(String str: list) {
-            try {
-                writer.write(str + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        writer.close();
-        System.out.println("\nResult:");
-        list.forEach(System.out::println);
-        System.out.println("result.size(): " + result.size());
-        System.out.println("Runtime: " + (System.currentTimeMillis() - from) / 1000  + " sec.");
-    }
+//    private static void output(long from, Set<String> set) throws IOException {
+//        Set<String> result = new TreeSet<>(set);
+//        List<String> list = new ArrayList<>();
+//        list.add(url);
+//        FileWriter writer = new FileWriter("src\\com\\company\\output.txt");
+//        for (String s : result) {
+//            String span = "";
+//            String shift = "    ";
+//            int coeff = s.split("/").length - url.split("/").length;
+//            for (int i = 0; i < coeff; i++) {
+//                span += shift ;
+//            }
+//            list.add(span + s);
+//        }
+//        for(String str: list) {
+//            try {
+//                writer.write(str + "\n");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        writer.close();
+//        System.out.println("\nResult:");
+//        list.forEach(System.out::println);
+//        System.out.println("result.size(): " + result.size());
+//        System.out.println("Runtime: " + (System.currentTimeMillis() - from) / 1000  + " sec.");
+//    }
 }
--- 11.13_TestForkJoinPool/src/com/company/Parser.java
+++ 11.13_TestForkJoinPool/src/com/company/Parser.java
@@ -14,31 +14,42 @@ public class Parser extends RecursiveTask<Set<String>> {
 
     private String url;
     private String prefix;
-    private ResultStore resultStore = new ResultStore();
-    private Set<Parser> taskSet = new HashSet<>();
-    private Set<Parser> childSet = new HashSet<>();
+    long from;
+    private ResultStore resultStore = new ResultStore(prefix + "/");
+//    private Set<String> taskSet = new HashSet<>();
+//    private Set<Parser> childSet = new HashSet<>();
+    private final int LIMIT = 1000;
 
-    public Parser(String url, String prefix) {
+    public Parser(long from, String url, String prefix) {
+        this.from = from;
         this.url = url.trim();
         this.prefix = prefix;
     }
 
     @Override
     protected Set<String> compute() {
-        if (resultStore.getResult().size() <= 500) {
-            parseAndGetTasksForChilds(); // создаем для данного url сет задач (дочерних ссылок для парсинга)
-            Set<Parser> subTaskSet = childSet; // передаем сет задач в метод
+        parseAndGetTasksForChilds(); // создаем для данного url сет задач (дочерних ссылок для парсинга)
+        Set<Parser> subTaskSet = resultStore.getChildParsers(); // передаем сет задач в метод
+        if (ResultStore.getResult().size() < LIMIT) {
             subTaskSet.stream().
-                    filter(task -> !taskSet.contains(task)). // проверяем task на уникальность
+                    filter(task -> !resultStore.getTaskSet().contains(task.url)). // проверяем task на уникальность
                     forEach(task -> {
-                taskSet.add(task); // добавляем новый выполненный task в реестр учета выполненных задач
-                System.out.println(Thread.currentThread().getName() + " -> task url: " + task.url + " - " + task.toString());//resultStore.getTaskSet().size());
-                resultStore.getResult().addAll(task.join());
+                resultStore.getTaskSet().add(task.url); // добавляем новый запущенный task в реестр учета задач
+                System.out.println(Thread.currentThread().getName() + " -> task url: " + task.url + "- result: " + resultStore.getResult().size());
+                ResultStore.getResult().addAll(task.join());
+
             });
         } else {
-            System.out.println("Set of references is full!");
+            System.out.println("Set of results is overloaded!");
+            try {
+                resultStore.output(from, ResultStore.getResult());
+                System.exit(0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-        return resultStore.getResult();
+        return ResultStore.getResult();
     }
 
     private Set<Parser> parseAndGetTasksForChilds() {
@@ -49,41 +60,47 @@ public class Parser extends RecursiveTask<Set<String>> {
             doc = Jsoup.connect(url).maxBodySize(3_000_000).userAgent("Mozilla").get();
             elements = doc.select("a");  //getElementsByAttribute("href");//
             for (Element element : elements) {
-                processElement(element);
+                if (ResultStore.getResult().size() < LIMIT) {
+                    processElement(element);
+                } else {
+                    System.out.println("Set of references is full!");
+                    break;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return childSet;
+        return resultStore.getChildParsers();
     }
 
     private void processElement(Element el) {
-        String fullRef;
-        String suffix = el.attr("href");
-        if (!suffix.startsWith("/")) {
-            suffix = "/".concat(suffix);
-        }
-        if (!suffix.contains("http") && suffix.length() > 3 && !suffix.contains("#") && !suffix.contains("@")
-                && !suffix.contains("tel:+") && !suffix.endsWith(".pdf") && !suffix.contains("/tg:/")) {
-            fullRef = prefix + suffix;
-            if (!resultStore.getResult().contains(fullRef)) {
-                Parser parser = new Parser(fullRef, prefix);
-                resultStore.getResult().add(fullRef);
-                parser.fork();
-                childSet.add(parser);
-                System.out.println(Thread.currentThread().getName() + " -> " + fullRef + " size of result: " +
-                        resultStore.getResult().size());
-            }
-        }
+//        String fullRef;
+//        String suffix = el.attr("href");
+//        if (!suffix.startsWith("/")) {
+//            suffix = "/".concat(suffix);
+//        }
+//        if (!suffix.contains("http") && suffix.length() > 3 && !suffix.contains("#") && !suffix.contains("@")
+//                && !suffix.contains("tel:+") && !suffix.endsWith(".pdf") && !suffix.contains("/tg:/")) {
+//            fullRef = prefix + suffix;
+//            if (!resultStore.getResult().contains(fullRef)) {
+//                Parser parser = new Parser(from, fullRef, prefix);
+//                resultStore.getResult().add(fullRef);
+//                parser.fork();
+//                childSet.add(parser);
+//                System.out.println(Thread.currentThread().getName() + " -> " + fullRef + " size of result: " +
+//                        resultStore.getResult().size());
+//            }
+//        }
 
         // For testing site:
-//        String attr = el.attr("abs:href");
-//        if (!attr.equals(prefix + "/") && !resultStore.getResult().contains(attr)) { // устраняем дублирующие ссылки
-//            Parser parser = new Parser(attr, prefix); // создаем Parser для каждой уникальной ссылки
-//            resultStore.getResult().add(attr);//resultStore.getResult().add(attr);
-//            parser.fork(); // запускаем асинхронное исполнение задачи в общем пуле потоков ForkJoinPool
-//            childSet.add(parser);// дабавляем объект Parser в коллекцию
-//        }
-//        System.out.println(Thread.currentThread().getName() + " -> " + fullRef + " - Result size: " + resultStore.getResult().size());
+        String attr = el.attr("abs:href");
+        if (attr.contains(prefix) && !attr.contains(".pdf") && !ResultStore.getResult().contains(attr) && !attr.contains("#")
+        && !attr.contains("@") && !attr.contains("tel:")) { // устраняем чуждые ссылки
+            Parser parser = new Parser(from, attr, prefix); // создаем Parser для каждой уникальной ссылки
+            ResultStore.getResult().add(attr);//resultStore.getResult().add(attr);
+            System.out.println(Thread.currentThread().getName() + " -> " + attr + " - Result size: " + ResultStore.getResult().size());
+            resultStore.getChildParsers().add(parser);// дабавляем объект Parser в коллекцию
+            parser.fork(); // запускаем асинхронное исполнение задачи в общем пуле потоков ForkJoinPool
+        }
     }
 }
