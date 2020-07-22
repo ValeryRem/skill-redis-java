2a326584773b99df13a0db36cc68b0556cd6d32b
Valery Remenyuk <remenyuk79@gmail.com>
2020-07-22 09:51:54 +0300

    Businees logic removed from TourisController to Storage.

--- 12.4_ToDoList/src/main/java/main/Storage.java
+++ 12.4_ToDoList/src/main/java/main/Storage.java
@@ -5,34 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class Storage {
     private int currentId = 1;
 
-    private Map<Integer, Tourist> touristsMap = new ConcurrentHashMap<>();
-    private List<String> seatList = new ArrayList<>();
+    private final Map<Integer, Tourist> touristsMap = new ConcurrentHashMap<>();
+    private final List<String> seatList = new ArrayList<>();
 //    Collection<Tourist> tourists = Storage.touristsMap.values();
 
     public Map<Integer, Tourist> getTouristsMap() {
         return this.touristsMap;
     }
-
-//    public ResponseEntity<Tourist> addTourist (Tourist tourist) {
-//        int id = currentId++;
-//        if(!seatList.contains(tourist.getSeat())) {
-//            tourist.setId(id);
-//            seatList.add(tourist.getSeat());
-//            touristsMap.put(id, tourist);
-//        } else {
-//            return new ResponseEntity<>(tourist, HttpStatus.NOT_ACCEPTABLE);
-//        }
-//        return new ResponseEntity<>(tourist, HttpStatus.OK);
-//    }
 
     public ResponseEntity<Tourist> addTourist (Tourist tourist) {
         System.out.printf("%s%s%s%s\n", "Add tourist: name: ", tourist.getName(), "  seat: ", tourist.getSeat());
@@ -47,6 +33,54 @@ public class Storage {
         System.out.println("Added tourist: id = " + id);
         return new ResponseEntity<>(tourist, HttpStatus.OK);
     }
+
+    public synchronized ResponseEntity<Tourist> putNewData(Integer id, String name, String seat, String birthday) {
+        Tourist tourist = getTourist(id);
+        if(name.length() > 0) {
+            Objects.requireNonNull(tourist).setName(name);
+        }
+        if(!seat.equals("0")) {
+            if (!seatList.contains(seat) || tourist.equals(seat)) {
+                tourist.setSeat(seat);
+            } else {
+                System.out.println("This seat is occupied. Change seat number!");
+            }
+        }
+        if(birthday.length() > 0) {
+            Objects.requireNonNull(tourist).setBirthday(birthday);
+        }
+//        touristRepository.save(Objects.requireNonNull(tourist));
+        return new ResponseEntity<>(tourist, HttpStatus.ACCEPTED);
+    }
+
+    public synchronized ResponseEntity<Integer> delete(Integer id) {
+        if (id > 0) {
+            if (touristsMap.containsKey(id)) {
+                String seat = touristsMap.get(id).getSeat();
+                System.out.println("Tourist removed: " + touristsMap.remove(id).getName());
+                System.out.println("SeatList before: "  + Arrays.toString(seatList.toArray()));
+                System.out.println("Connected seat to remove: " + seat);
+                seatList.remove(seat);
+                System.out.println("SeatList after: "  + Arrays.toString(seatList.toArray()));
+            } else {
+                return new ResponseEntity<>(id, HttpStatus.NOT_FOUND);
+            }
+        } else {
+            touristsMap.clear();
+        }
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Tourist> get(Integer id) {
+        Tourist tourist = getTourist(id);
+//        return ResponseEntity.of(Optional.ofNullable(tourist));
+//    }
+        if (tourist == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity<>(tourist, HttpStatus.OK);
+    }
+
 
     public List<String> getSeatList() {
         return seatList;
--- 12.4_ToDoList/src/main/java/main/TouristController.java
+++ 12.4_ToDoList/src/main/java/main/TouristController.java
@@ -2,13 +2,10 @@ package main;
 
 import main.model.Tourist;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Objects;
 
 @RestController
 public class TouristController {
@@ -29,53 +26,17 @@ public class TouristController {
     }
 
     @GetMapping("/tourists/{id}")
-    public ResponseEntity<Tourist> get(@PathVariable("id") Integer id) {
-        Tourist tourist = storage.getTourist(id);
-//        return ResponseEntity.of(Optional.ofNullable(tourist));
-//    }
-            if (tourist == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-        return new ResponseEntity<>(tourist, HttpStatus.OK);
+    public ResponseEntity<Tourist> getTourist (@PathVariable("id") Integer id) {
+        return storage.get(id);
     }
 
     @PutMapping("/tourists/{id}")
-    public synchronized ResponseEntity<Tourist> putNewData(@PathVariable("id")Integer id, String name, String seat, String birthday) {
-        Tourist tourist = storage.getTourist(id);
-        if(name.length() > 0) {
-            Objects.requireNonNull(tourist).setName(name);
-
-        }
-        if(!seat.equals("0")) {
-            if (!storage.getSeatList().contains(seat) || tourist.getSeat().equals(seat)) {
-                tourist.setSeat(seat);
-            } else {
-                System.out.println("This seat is occupied. Change seat number!");
-            }
-        }
-        if(birthday.length() >0) {
-            Objects.requireNonNull(tourist).setBirthday(birthday);
-        }
-//        touristRepository.save(Objects.requireNonNull(tourist));
-        return new ResponseEntity<>(tourist, HttpStatus.ACCEPTED);
+    public ResponseEntity<Tourist> putChanges(@PathVariable("id")Integer id, String name, String seat, String birthday) {
+        return storage.putNewData(id, name, seat, birthday);
     }
 
     @DeleteMapping("/tourists/{id}")
-    public synchronized ResponseEntity<Integer> delete(@PathVariable("id") Integer id) {
-        if (id > 0) {
-            if (storage.getTouristsMap().containsKey(id)) {
-                String seat = storage.getTouristsMap().get(id).getSeat();
-                System.out.println("Tourist removed: " + storage.getTouristsMap().remove(id).getName());
-                System.out.println("SeatList before: "  + Arrays.toString(storage.getSeatList().toArray()));
-                System.out.println("Connected seat to remove: " + seat);
-                storage.getSeatList().remove(seat);
-                System.out.println("SeatList after: "  + Arrays.toString(storage.getSeatList().toArray()));
-            } else {
-                return new ResponseEntity<>(id, HttpStatus.NOT_FOUND);
-            }
-        } else {
-            storage.getTouristsMap().clear();
-        }
-        return new ResponseEntity<>(id, HttpStatus.OK);
+    public ResponseEntity<Integer> deleteTourist(@PathVariable("id") Integer id) {
+        return storage.delete(id);
     }
 }
