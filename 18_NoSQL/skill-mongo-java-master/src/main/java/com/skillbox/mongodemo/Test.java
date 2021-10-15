7382b0cbc592dc1b3c41d42a5eedca57e5ca8a29
Valery Remenyuk <remenyuk79@gmail.com>
2021-10-15 19:53:36 +0300

    MongoDB test #2.

--- 18_NoSQL/skill-mongo-java-master/src/main/java/com/skillbox/mongodemo/Test.java
+++ 18_NoSQL/skill-mongo-java-master/src/main/java/com/skillbox/mongodemo/Test.java
@@ -5,10 +5,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.ListDatabasesIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 import com.mongodb.client.model.Collation;
 import org.bson.BsonDocument;
 import org.bson.Document;
@@ -25,6 +22,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Consumer;
+import static com.mongodb.client.model.Filters.*;
 
 public class Test {
 
@@ -38,31 +36,38 @@ public class Test {
 
         // Удалим из нее все документы
         collection.drop();
-//        System.out.println("Cunt: " + collection.countDocuments());
 
         try {
             CSVReader reader = new CSVReader(new FileReader("mongo.csv"), ',' , '"' , 0);
             String[] nextLine;
             while ((nextLine = reader.readNext()) != null) {
                     Document document = new Document()
-                            .append("Name", nextLine[0])
-                            .append("Age", nextLine[1])
-                            .append("Courses", nextLine[2]);
+                            .append("Name", nextLine[0].trim())
+                            .append("Age", nextLine[1].trim())
+                            .append("Courses", nextLine[2].trim());
                     collection.insertOne(document);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         System.out.println("Count of students in the collection: " + collection.countDocuments());
-
-        BasicDBObject query = new BasicDBObject();
-        query.put("Age", new BasicDBObject("$gt", 40));
-        FindIterable<Document> iterable = collection.find(query);
-        MongoCollection<Document> collectionOver40 = database.getCollection("studentsOver40");
-        while (iterable.iterator().hasNext()) {
-            Document studentOver40 = iterable.iterator().next();
-            collectionOver40.insertOne(studentOver40);
+//        int i = 0;
+//        for (Document d : collection.find()) {
+//            ++i;
+//            System.out.println(i + ". " + d.toJson());
+//        }
+//        BasicDBObject query = new BasicDBObject();
+//        var query = new BasicDBObject("Age", new BasicDBObject("$gt", 40));
+//        collection.find(query).forEach((Consumer<Document>) System.out::println);
+        System.out.println("\nStudents of Age over 40: ");
+        int i = 0;
+        for (Document d :  collection.find(and(gt("age", 40)))) {
+            ++i;
+            System.out.println(i + ". " + d);
         }
-        System.out.println("Count of students over40: " + collectionOver40.countDocuments());
+        System.out.println("Count of students over 40: " + i);
+        collection.find(and(gt("Age", 40))).forEach((Consumer<Document>) System.out::println);
+
+
     }
 }
