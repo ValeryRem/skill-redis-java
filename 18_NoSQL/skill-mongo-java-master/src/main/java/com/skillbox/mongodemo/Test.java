d0ca65379fe46b7642a78d2b725e71f258e78007
Valery Remenyuk <remenyuk79@gmail.com>
2021-10-17 11:21:28 +0300

    MongoDB test #3: full version.

--- 18_NoSQL/skill-mongo-java-master/src/main/java/com/skillbox/mongodemo/Test.java
+++ 18_NoSQL/skill-mongo-java-master/src/main/java/com/skillbox/mongodemo/Test.java
@@ -2,25 +2,15 @@ package com.skillbox.mongodemo;
 
 import au.com.bytecode.opencsv.CSVReader;
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.*;
-import com.mongodb.client.model.Collation;
-import org.bson.BsonDocument;
 import org.bson.Document;
-import org.bson.conversions.Bson;
-import org.bson.json.JsonMode;
-import org.bson.json.JsonWriterSettings;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 import java.util.function.Consumer;
 import static com.mongodb.client.model.Filters.*;
 
@@ -28,12 +18,9 @@ public class Test {
 
     public static void main(String[] args) throws IOException {
         MongoClient mongoClient = new MongoClient( "127.0.0.1" , 27017 );
-
         MongoDatabase database = mongoClient.getDatabase("mongo-students");
-
         // Создаем коллекцию
         MongoCollection<Document> collection = database.getCollection("students");
-
         // Удалим из нее все документы
         collection.drop();
 
@@ -43,7 +30,7 @@ public class Test {
             while ((nextLine = reader.readNext()) != null) {
                     Document document = new Document()
                             .append("Name", nextLine[0].trim())
-                            .append("Age", nextLine[1].trim())
+                            .append("Age", Integer.parseInt(nextLine[1].trim()))
                             .append("Courses", nextLine[2].trim());
                     collection.insertOne(document);
             }
@@ -59,15 +46,23 @@ public class Test {
 //        BasicDBObject query = new BasicDBObject();
 //        var query = new BasicDBObject("Age", new BasicDBObject("$gt", 40));
 //        collection.find(query).forEach((Consumer<Document>) System.out::println);
-        System.out.println("\nStudents of Age over 40: ");
-        int i = 0;
-        for (Document d :  collection.find(and(gt("age", 40)))) {
-            ++i;
-            System.out.println(i + ". " + d);
+
+        int k = 0;
+        for (Document d : collection.find(and(gt("Age", 40)))) {
+            k++;
         }
-        System.out.println("Count of students over 40: " + i);
+        System.out.println("Count of students over 40: " + k);
+        System.out.println("\nStudents of Age over 40: ");
         collection.find(and(gt("Age", 40))).forEach((Consumer<Document>) System.out::println);
 
+        FindIterable<Document> cursor = collection.find();
+        Document document = cursor.sort(new BasicDBObject("Age", 1)).limit(1).iterator().tryNext();
+        if (document != null) {
+            System.out.println("\nA student of the yangest Age: " + document.get("Name"));
+        }
 
+        if (document != null) {
+            System.out.print("\nCourses of the yangest student: " + document.get("Courses"));
+        }
     }
 }
