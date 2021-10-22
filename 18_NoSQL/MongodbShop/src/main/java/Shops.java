f7a5fa950bca3cf95b2e5ff393b8557b825508ce
Valery Remenyuk <remenyuk79@gmail.com>
2021-10-22 14:27:38 +0300

    Mongo Shops #2.

--- 18_NoSQL/MongodbShop/src/main/java/Shops.java
+++ 18_NoSQL/MongodbShop/src/main/java/Shops.java
@@ -3,21 +3,33 @@ import org.bson.Document;
 import java.util.*;
 
 public class Shops {
-    private static Integer NUMBER_OF_SHOPS = 5;
-    private static Integer NUMBER_OF_GOODS = 100;
-    private static Integer MIN_PRICE = 1;
-    private static Integer MAX_PRICE = 1000;
+    private static final int NUMBER_OF_SHOPS = 5;
+    private static final int NUMBER_OF_GOODS = 100;
+    private static final int MIN_PRICE = 1;
+    private static final int MAX_PRICE = 1000;
+//    private static ShopsGenerator shopsGenerator;
 
     public static void main(String[] args) {
         ShopsGenerator shopsGenerator = new ShopsGenerator(NUMBER_OF_SHOPS, NUMBER_OF_GOODS, MIN_PRICE, MAX_PRICE);
-        Collection<MongoCollection<Document>> shopsCollection = shopsGenerator.getShopsCollection();
-        System.out.println("shopsCollection size: " + shopsCollection.size());
-        for (MongoCollection<Document> shops: shopsCollection) {
-            System.out.println("Number of goods: " + shops.countDocuments());
-            for (Document shopDoc: shops.find()) {
-                System.out.println(shopDoc);
+        ArrayList<MongoCollection<Document>> shopsCollection = shopsGenerator.getShopsCollection();
+//        for (MongoCollection<Document> shop: shopsCollection) {
+//            System.out.println(shop.getNamespace());
+//            for (Document shopDoc: shop.find()) {
+//                System.out.println(shopDoc);
+//            }
+//        }
+
+        MongoCollection<Document> statistics;
+        try {
+            statistics = shopsGenerator.getStatistics(shopsCollection);
+            for (Document shopStat: statistics.find()) {
+                System.out.println(shopStat);
             }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
         }
+
+
     }
 }
 
--- 18_NoSQL/MongodbShop/src/main/java/ShopsGenerator.java
+++ 18_NoSQL/MongodbShop/src/main/java/ShopsGenerator.java
@@ -1,20 +1,22 @@
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.BsonField;
+import com.mongodb.client.model.Projections;
 import org.bson.Document;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
+import static com.mongodb.client.model.Filters.*;
 
 public class ShopsGenerator {
 
-    private Integer NUMBER_OF_SHOPS;
-    private Integer NUMBER_OF_GOODS;
-    private Integer MAX_PRICE;
-    private Integer MIN_PRICE;
+    private final int NUMBER_OF_SHOPS;
+    private final int NUMBER_OF_GOODS;
+    private final int MAX_PRICE;
+    private final int MIN_PRICE;
 
     public ShopsGenerator(Integer NUMBER_OF_SHOPS, Integer NUMBER_OF_GOODS, Integer MIN_PRICE, Integer MAX_PRICE) {
         this.NUMBER_OF_SHOPS = NUMBER_OF_SHOPS;
@@ -26,85 +28,56 @@ public class ShopsGenerator {
     private final MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
     private final MongoDatabase database = mongoClient.getDatabase("mongo-shops");
 
-    // Метод создния коллекции МАГАЗИН
-    private MongoCollection<Document> addShopCollection(String shopName) {
-        return database.getCollection(shopName);
-    }
-    // Метод создния коллекции ТОВАРЫ
-    private MongoCollection<Document> addGoodsCollection(String goods) {
-        return database.getCollection(goods);
-    }
-    // Метод добавления 1 товара в МАГАЗИН
-    void addGoodsToShop(MongoCollection<Document> shopName, Document doc) {
-        shopName.insertOne(doc);
-    }
+    // Создаем базовую коллекцию тоаров GoodsCollection
+    private MongoCollection<Document> goods = database.getCollection("GoodsCollection");
     // Метод создания сети магазинов с запасами товаров
-    GoodsGenerator goodsGenerator = new GoodsGenerator();
-    Map<String, Integer> products = goodsGenerator.getProductsMap(NUMBER_OF_GOODS, MIN_PRICE, MAX_PRICE);
-    MongoCollection<Document> goods = addGoodsCollection("goodsCollection");
+    public ArrayList<MongoCollection<Document>> getShopsCollection() {
+        ArrayList<MongoCollection<Document>> shopsCollection = new ArrayList<>();
 
-    public Collection<MongoCollection<Document>> getShopsCollection() {
-        Collection<String> productNames = products.keySet();
-        Collection<MongoCollection<Document>> shopsCollection = new ArrayList<>();
+        // Наполняем базовую коллекцию goods тоарами в кол-ве NUMBER_OF_GOODS
+        getGoods(NUMBER_OF_GOODS, MIN_PRICE, MAX_PRICE);
         for (int i = 1; i <= NUMBER_OF_SHOPS; i++) {
-            MongoCollection<Document> shop = addShopCollection("Shop_" + i);
+            //Создаем новый магазин
+            MongoCollection<Document> shop = database.getCollection("Shop_" + i);
+            shop.drop();
             int numberOfGoodsHere = (int) (Math.random() * NUMBER_OF_GOODS);
-            FindIterable<Document> iterable = goods.find().limit(numberOfGoodsHere);
-            for (String key: productNames.stream().limit(numberOfGoodsHere).collect(Collectors.toList())) {
-                Document document = new Document();
-                document.append("name", key)
-                        .append("price", iterable.iterator().next().get(key)); //products.get(key));
-                addGoodsToShop(shop, document);
+            // Определем набор товаров для данного магазина - первые numberOfGoodsHere из базовой коллекции товаров
+            FindIterable<Document> iterableGoods = goods.find().limit(numberOfGoodsHere);
+            for (Document d: iterableGoods) {
+                shop.insertOne(d);
             }
             shopsCollection.add(shop);
         }
         return shopsCollection;
     }
-
-    public Integer getNUMBER_OF_SHOPS() {
-        return NUMBER_OF_SHOPS;
+    // Метод наполнения коллекции goods тоарами в кол-ве NUMBER_OF_GOODS со случайными ценами
+    private void getGoods (int NUMBER_OF_GOODS, int MIN_PRICE, int MAX_PRICE){
+        int price;
+        String name;
+        for (int k = 1; k <= NUMBER_OF_GOODS; k++) {
+            price = (int) (MIN_PRICE + (MAX_PRICE - MIN_PRICE) * Math.random());
+            name = "Item_" + k;
+            Document document = new Document();
+            document.append("name", name).append("price", price);
+            goods.insertOne(document);
+        }
     }
 
-    public void setNUMBER_OF_SHOPS(Integer NUMBER_OF_SHOPS) {
-        this.NUMBER_OF_SHOPS = NUMBER_OF_SHOPS;
+    public MongoCollection<Document> getStatistics(ArrayList<MongoCollection<Document>> listOfShops) throws NoSuchFieldException {
+        MongoCollection<Document> result = database.getCollection("Statistics");
+        result.drop();
+        for (MongoCollection<Document> shop : listOfShops) {
+            Document document = new Document();
+            //Находим общее количество наименований товаров в магазине
+            document.append("shop", shop.getNamespace()).append("countOfGoods", shop.countDocuments());
+            //Находим количество товаров дешевле 100 рублей в магазине.
+            int countOfLess100 = 0;
+            for (Document d : shop.find(and(lt("price", 100)))) {
+                countOfLess100++;
+            }
+            document.append("count of prices less 100", countOfLess100);
+            result.insertOne(document);
+        }
+        return result;
     }
-
-    public Integer getNUMBER_OF_GOODS() {
-        return NUMBER_OF_GOODS;
-    }
-
-    public void setNUMBER_OF_GOODS(Integer NUMBER_OF_GOODS) {
-        this.NUMBER_OF_GOODS = NUMBER_OF_GOODS;
-    }
-
-    public Integer getMAX_PRICE() {
-        return MAX_PRICE;
-    }
-
-    public void setMAX_PRICE(Integer MAX_PRICE) {
-        this.MAX_PRICE = MAX_PRICE;
-    }
-
-    public Integer getMIN_PRICE() {
-        return MIN_PRICE;
-    }
-
-    public void setMIN_PRICE(Integer MIN_PRICE) {
-        this.MIN_PRICE = MIN_PRICE;
-    }
-    //    // Создаем коллекции разных магазинов и очистим их
-//    MongoCollection<Document> shopNine = addShopCollection("shopNine");
-    // Создаем коллекцию ТОВАРЫ и удалим из нее все документы
-//    MongoCollection<Document> goods = addGoodsCollection("goodsCollection");
-    // Используем генератор коллекции товаров goods из класса GoodsGenerator
-
-
-
-    // Делаем выборку первых N товаров из коллекции goods для магазина shopNine, где N определено случайным образом
-
-    // Выводим в консоль результат - коллекцию товаров в shopNine
-//    FindIterable<Document> iterableShopNine = shopNine.find();
-//        for (Document d: iterableShopNine) {
-//        System.out.println(d.toJson());
-//    }
 }
