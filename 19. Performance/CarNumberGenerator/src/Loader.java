2288f11620f537f95a47c38ad91ab18d617119f5
Valery Remenyuk <remenyuk79@gmail.com>
2021-11-01 22:44:43 +0300

    CarNumberGenerator with separate files written.

--- 19. Performance/CarNumberGenerator/src/Loader.java
+++ 19. Performance/CarNumberGenerator/src/Loader.java
@@ -1,15 +1,37 @@
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class Loader {
+    private static List<String> filenameList = new LinkedList<>();
     public static void main(String[] args) {
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newScheduledThreadPool(4);
-        for (int i = 0; i < 10; i++) {
-            Task task = new Task("task " + i);
+        long start = new Task().start;
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
+        for (int i = 1; i <= 4; i++) {
+            Task task = new Task(i);
+            filenameList.add(task.getFileNameRoot() + i + ".txt");
             executor.execute(task);
         }
         executor.shutdown();
-    }
 
+        try(OutputStream out = Files.newOutputStream(Paths.get(new Task().getFileNameRoot()  + ".txt"),
+                StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+            for (String inFileName : filenameList) {
+                Files.copy(Paths.get(inFileName), out);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Проверяем завершение записи частных резултатов в полный файл и выводим общее время
+        if(new Task().checkFile()) {
+            System.out.println(("Total duration: " +  (System.currentTimeMillis() - start) + " ms"));
+        }
+    }
 }
--- 19. Performance/CarNumberGenerator/src/Task.java
+++ 19. Performance/CarNumberGenerator/src/Task.java
@@ -1,24 +1,41 @@
+import java.io.File;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.TimeUnit;
 
 public class Task implements Runnable {
-    private String name;
+    private final String fileNameRoot = "res/numbers_";
+    private int numebrOfTask;
+    private String taskName;
+    private final char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+    public long start = System.currentTimeMillis();
+    public Task() {}
 
-    public Task(String name)
+    public Task(int numebrOfTask)
     {
-        this.name = name;
+        this.numebrOfTask = numebrOfTask;
     }
 
-    public String getName() {
-        return name;
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setNumebrOfTask(int numebrOfTask) {
+        this.numebrOfTask = numebrOfTask;
+    }
+
+    public String getFileNameRoot() {
+        return fileNameRoot;
     }
 
     @Override
-    public void run()
-    {
+    public void run() {
         try
         {
-            generateCarNumber();
+            generateCarNumber(numebrOfTask);
             TimeUnit.MILLISECONDS.sleep(100);
         }
         catch (Exception e)
@@ -27,16 +44,16 @@ public class Task implements Runnable {
         }
     }
 
-    private void generateCarNumber() throws Exception{
-        long start = System.currentTimeMillis();
-        PrintWriter printWriter = new PrintWriter("res/numbers.txt");
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+    private void generateCarNumber(int i) throws Exception {
+//        long start = System.currentTimeMillis();
+        PrintWriter printWriter = new PrintWriter(fileNameRoot + i + ".txt");
+        char[] subset = {letters[3*(i-1)], letters[1+3*(i-1)], letters[2+3*(i-1)]};
         int regionCode = 199;
         for (int regions = 1; regions < 10; regions++) {
             for (int number = 1; number < 1000; number++) {
-                for (char firstLetter : letters) {
-                    for (char secondLetter : letters) {
-                        for (char thirdLetter : letters) {
+                for (char firstLetter : subset) {
+                    for (char secondLetter : subset) {
+                        for (char thirdLetter : subset) {
                             StringBuilder stringBuilder = new StringBuilder();
                             stringBuilder.append(firstLetter);
                             stringBuilder.append(padNumber(number, 3));
@@ -52,10 +69,12 @@ public class Task implements Runnable {
         }
         printWriter.flush();
         printWriter.close();
-        System.out.println((getName() + ": " + Thread.currentThread().getName() + ": " + (System.currentTimeMillis() - start) + " ms"));
+
+
+        System.out.println(("Task " + i + ": " + Thread.currentThread().getName()) + ": " + (System.currentTimeMillis() - start) + " ms");
     }
 
-    private static String padNumber(int number, int numberLength) {
+    private String padNumber(int number, int numberLength) {
         StringBuilder builder = new StringBuilder();
         builder.append(number);
         int padSize = numberLength - builder.toString().length();
@@ -64,5 +83,9 @@ public class Task implements Runnable {
             builder.append(padSize - numberLength);
         }
         return builder.toString();
+    }
+
+    public boolean checkFile () {
+        return  new File(getFileNameRoot()  + ".txt").isFile();
     }
 }
