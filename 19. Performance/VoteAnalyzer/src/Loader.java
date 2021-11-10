c8d68dfc6ec029b52232d929d6ae67d3643f2668
Valery Remenyuk <remenyuk79@gmail.com>
2021-11-10 23:45:00 +0300

    Used memory for SАX-parser.

--- 19. Performance/VoteAnalyzer/src/Loader.java
+++ 19. Performance/VoteAnalyzer/src/Loader.java
@@ -5,6 +5,8 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,75 +24,82 @@ public class Loader
     {
         String fileName = "res/data-18M.xml";
         long freeMemory = Runtime.getRuntime().freeMemory();
-        System.out.println("***** starting freeMemory: " + freeMemory); //
-        parseFile(fileName);
+        System.out.println("*** free memory beginning: " + freeMemory);
 
-        //Printing results
-        System.out.println("Voting station work times: ");
-        for(Integer votingStation : voteStationWorkTimes.keySet())
-        {
-            WorkTime workTime = voteStationWorkTimes.get(votingStation);
-            System.out.println("\t" + votingStation + " - " + workTime);
-        }
-
-        System.out.println("Duplicated voters: ");
-        for(Voter voter : voterCounts.keySet())
-        {
-            Integer count = voterCounts.get(voter);
-            if(count > 1) {
-                System.out.println("\t" + voter + " - " + count);
-            }
-        }
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        SAXParser parser = saxParserFactory.newSAXParser();
+        XMLHandler xmlHandler = new XMLHandler();
+        parser.parse(new File(fileName), xmlHandler);
+        xmlHandler.printDoubledVisits();
+//        System.out.println("***** starting freeMemory: " + freeMemory); //
+//        parseFile(fileName);
+//
+//        //Printing results
+//        System.out.println("Voting station work times: ");
+//        for(Integer votingStation : voteStationWorkTimes.keySet())
+//        {
+//            WorkTime workTime = voteStationWorkTimes.get(votingStation);
+//            System.out.println("\t" + votingStation + " - " + workTime);
+//        }
+//
+//        System.out.println("Duplicated voters: ");
+//        for(Voter voter : voterCounts.keySet())
+//        {
+//            Integer count = voterCounts.get(voter);
+//            if(count > 1) {
+//                System.out.println("\t" + voter + " - " + count);
+//            }
+//        }
         long usedMemory = freeMemory - Runtime.getRuntime().freeMemory();
         System.out.println("usedMemory : " + usedMemory);
     }
 
-    private static void parseFile(String fileName) throws Exception
-    {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document doc = db.parse(new File(fileName));
-
-        findEqualVoters(doc);
-        fixWorkTimes(doc);
-    }
-
-    private static void findEqualVoters(Document doc) throws Exception
-    {
-        NodeList voters = doc.getElementsByTagName("voter");
-        int votersCount = voters.getLength();
-        for(int i = 0; i < votersCount; i++)
-        {
-            Node node = voters.item(i);
-            NamedNodeMap attributes = node.getAttributes();
-
-            String name = attributes.getNamedItem("name").getNodeValue();
-            Date birthDay = birthDayFormat.parse(attributes.getNamedItem("birthDay").getNodeValue());
-
-            Voter voter = new Voter(name, birthDay);
-            Integer count = voterCounts.get(voter);
-            voterCounts.put(voter, count == null ? 1 : count + 1);
-        }
-    }
-
-    private static void fixWorkTimes(Document doc) throws Exception
-    {
-        NodeList visits = doc.getElementsByTagName("visit");
-        int visitCount = visits.getLength();
-        for(int i = 0; i < visitCount; i++)
-        {
-            Node node = visits.item(i);
-            NamedNodeMap attributes = node.getAttributes();
-
-            Integer station = Integer.parseInt(attributes.getNamedItem("station").getNodeValue());
-            Date time = visitDateFormat.parse(attributes.getNamedItem("time").getNodeValue());
-            WorkTime workTime = voteStationWorkTimes.get(station);
-            if(workTime == null)
-            {
-                workTime = new WorkTime();
-                voteStationWorkTimes.put(station, workTime);
-            }
-            workTime.addVisitTime(time.getTime());
-        }
-    }
+//    private static void parseFile(String fileName) throws Exception
+//    {
+//        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder db = dbf.newDocumentBuilder();
+//        Document doc = db.parse(new File(fileName));
+//
+//        findEqualVoters(doc);
+//        fixWorkTimes(doc);
+//    }
+//
+//    private static void findEqualVoters(Document doc) throws Exception
+//    {
+//        NodeList voters = doc.getElementsByTagName("voter");
+//        int votersCount = voters.getLength();
+//        for(int i = 0; i < votersCount; i++)
+//        {
+//            Node node = voters.item(i);
+//            NamedNodeMap attributes = node.getAttributes();
+//
+//            String name = attributes.getNamedItem("name").getNodeValue();
+//            Date birthDay = birthDayFormat.parse(attributes.getNamedItem("birthDay").getNodeValue());
+//
+//            Voter voter = new Voter(name, birthDay);
+//            Integer count = voterCounts.get(voter);
+//            voterCounts.put(voter, count == null ? 1 : count + 1);
+//        }
+//    }
+//
+//    private static void fixWorkTimes(Document doc) throws Exception
+//    {
+//        NodeList visits = doc.getElementsByTagName("visit");
+//        int visitCount = visits.getLength();
+//        for(int i = 0; i < visitCount; i++)
+//        {
+//            Node node = visits.item(i);
+//            NamedNodeMap attributes = node.getAttributes();
+//
+//            Integer station = Integer.parseInt(attributes.getNamedItem("station").getNodeValue());
+//            Date time = visitDateFormat.parse(attributes.getNamedItem("time").getNodeValue());
+//            WorkTime workTime = voteStationWorkTimes.get(station);
+//            if(workTime == null)
+//            {
+//                workTime = new WorkTime();
+//                voteStationWorkTimes.put(station, workTime);
+//            }
+//            workTime.addVisitTime(time.getTime());
+//        }
+//    }
 }
---
+++ 19. Performance/VoteAnalyzer/src/XMLHandler.java
@@ -0,0 +1,55 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class XMLHandler extends DefaultHandler {
+    private Voter voter;// = new Voter();
+
+    private static final SimpleDateFormat birhdayFormat = new SimpleDateFormat("yyyy.MM.dd");
+    private HashMap<Voter, Integer> voterCounts;
+    public XMLHandler () {
+        voterCounts = new HashMap<>();
+    }
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        try {
+            if (qName.equals("voter") && voter == null) {
+                Date birthday = birhdayFormat.parse(attributes.getValue("birthDay"));
+                voter = new Voter(attributes.getValue("name"), birthday);
+//                System.out.println("started: " + qName);
+            } else if (qName.equals("visit") && voter !=null) {
+                int count = voterCounts.getOrDefault(voter, 0);
+                voterCounts.put(voter, count + 1);
+            }
+        } catch (ParseException e) {
+                e.printStackTrace();
+            }
+//        super.startElement(uri, localName, qName, attributes);
+
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if(qName.equals("voter")) {
+        voter = null;
+        }
+    }
+//        super.endElement(uri, localName, qName);
+//        System.out.println("ended: " + qName);
+
+    public void printDoubledVisits(){
+        for (Voter v: voterCounts.keySet()) {
+            int count = voterCounts.get(v);
+            if(count > 1){
+                System.out.println(v.toString() + " - " + count);
+            }
+
+        }
+    }
+}
+
